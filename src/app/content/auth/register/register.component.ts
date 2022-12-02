f8561ec37bdbac2ba7fb16ff8d5abb35ac22ed77
Salma Chaaban <schaaban@my.centennialcollege.ca>
2022-12-02 19:19:04 +0300

    Registration Complete

--- src/app/content/auth/register/register.component.ts
+++ src/app/content/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    firstName: null,
+    lastName: null,
+    emailAddress: null,
+    username: null,
+    password: null,
+    confirmPassword: null
+  }
+
+  isSignedUpFailed = false;
+  errorMessage = "";
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    const {
+      firstName,
+      lastName,
+      emailAddress,
+      username,
+      password
+    } = this.form
+
+    this.authService.register(username, password, emailAddress, firstName + lastName)
+    .subscribe({
+      next: data => {
+        console.log(data);
+        this.isSignedUpFailed = false;
+        this.goToHome();
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isSignedUpFailed = true;
+      }
+    })
+  }
+
+  goToHome(): void {
+    this.router.navigate(['/login']);
   }
 
 }
